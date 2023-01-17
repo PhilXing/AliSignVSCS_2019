2e56bb62ac65f9afcf9d81667f68e3dead45fbe6
Phil.Xing <phil.xing@adlinktech.com>
2023-01-17 14:29:28 +0800

    Fix empty working folder issue.

--- AliSign/Form1.cs
+++ AliSign/Form1.cs
@@ -241,33 +241,13 @@ namespace AliSign
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
                 textBoxWorkingFolder.Text = folderBrowserDialog1.SelectedPath;
+                ResetInputFiles();
             }
         }
 
-        private void textBoxWorkingFolder_TextChanged(object sender, EventArgs e)
-        {
-           //System.Windows.Forms.TextBox textBox = (System.Windows.Forms.TextBox)sender;
-           //string text = textBox.Text;
-
-           // // Check if the specified folder exists
-           // if (Directory.Exists(text))
-           // {
-           //     ResetInputFiles();
-           // }
-        }
-
-        //public string CurrentWorkingFolder = "";
         private void textBoxWorkingFolder_Leave(object sender, EventArgs e)
         {
-            System.Windows.Forms.TextBox textBox = (System.Windows.Forms.TextBox)sender;
-            string text = textBox.Text;
-
-            // Check if the specified folder exists
-            if (Directory.Exists(text)) // && (text!=CurrentWorkingFolder))
-            {
-                ResetInputFiles();
-                //CurrentWorkingFolder = text;
-            }
+            ResetInputFiles();
         }
 
         private void ResetInputFiles()
